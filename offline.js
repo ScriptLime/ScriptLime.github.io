d176e9c7233308ad6e460f020c3829de4a1ff28c
ScriptLime <94577517+ScriptLime@users.noreply.github.com>
2021-11-18 05:52:04 +0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1637193034,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/İmportx15-sheet0.png",
+		"images/git-sheet0.png",
+		"images/ins-sheet0.png",
+		"images/web-sheet0.png",
+		"images/sprite-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"style.css"
+	]
+}
